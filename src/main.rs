5cc5f61ba296a13f1460196cf37b4964b3a64df8
ErneDainesi <edainesi@fi.uba.ar>
2023-08-22 18:30:25 -0300

    add post end point

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
-use actix_web::{get, http::header::ContentType, App, HttpResponse, HttpServer};
+use actix_web::{
+    get, http::header::ContentType, post, web, App, HttpRequest, HttpResponse, HttpServer,
+};
 use leptos::*;
-use todo_rust::todo::Todo;
+use todo_rust::todo::{Todo, TodoItem, TodosForm, TodosList};
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+struct FormData {
+    title: String,
+    description: String,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct AppState {
+    id_counter: usize,
+}
+
+fn get_example_data() -> Vec<TodoItem> {
+    vec![
+        TodoItem {
+            id: 1,
+            title: "Prueba".to_string(),
+            description: "Prueba desc".to_string(),
+        },
+        TodoItem {
+            id: 2,
+            title: "Otra Prueba".to_string(),
+            description: "Otra prueba desc".to_string(),
+        },
+    ]
+}
 
 #[get("/")]
 async fn index() -> HttpResponse {
@@ -15,10 +43,28 @@ async fn index() -> HttpResponse {
             </head>
             <body>
                 <h1>Todo!</h1>
-                <section>
-                    <Todo/>
-                </section>
+                <TodosForm/>
+                <TodosList todos = get_example_data() />
             </body>
+        }
+    });
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(html)
+}
+
+#[post("/")]
+async fn add_todo(app_data: web::Data<AppState>, form: web::Form<FormData>) -> HttpResponse {
+    let id = app_data.id_counter + 1; // this doesn't really work, but ill use sql ids later
+    let form_data = TodoItem {
+        id,
+        title: form.title.to_owned(),
+        description: form.description.to_owned(),
+    };
+    let html = leptos::ssr::render_to_string(|cx| {
+        view! {
+            cx,
+            <Todo data = form_data />
         }
     });
     HttpResponse::Ok()
@@ -28,8 +74,13 @@ async fn index() -> HttpResponse {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(add_todo)
+            .app_data(web::Data::new(AppState { id_counter: 0 }))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
