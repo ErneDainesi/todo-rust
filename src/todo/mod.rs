098e7cbfc8290b9e1d0e8a689465d38771b0a96b
ErneDainesi <edainesi@fi.uba.ar>
2023-08-22 18:30:13 -0300

    add todo list and form

--- src/todo/mod.rs
+++ src/todo/mod.rs
@@ -1,12 +1,57 @@
 use leptos::*;
 
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub struct TodoItem {
+    pub id: usize,
+    pub title: String,
+    pub description: String,
+}
+
 #[component]
-pub fn Todo(cx: Scope) -> impl IntoView {
+pub fn Todo(cx: Scope, data: TodoItem) -> impl IntoView {
     view! {
         cx,
         <div>
-            <h3>This will be a todo item</h3>
-            <p>bla bla bla...</p>
+            <h3>{ data.title }</h3>
+            <p>{ data.description }</p>
         </div>
+    }
+}
+
+#[component]
+pub fn TodosForm(cx: Scope) -> impl IntoView {
+    view! {
+        cx,
+        <form
+            class="todo-form"
+            hx-post="/"
+            hx-target="#list"
+            hx-trigger="submit"
+            hx-swap="beforeend"
+        >
+            <input placeholder="Title" type="text" name="title"/>
+            <input placeholder="Description" type="text" name="description"/>
+            <button type="submit">Add</button>
+        </form>
+    }
+}
+
+#[component]
+pub fn TodosList(cx: Scope, todos: Vec<TodoItem>) -> impl IntoView {
+    let (todos, _) = create_signal::<Vec<TodoItem>>(cx, todos);
+    view! {
+        cx,
+        <section id="list">
+            <For
+                each = move || todos.get()
+                key = |item| item.id
+                view = move |cx, item: TodoItem| {
+                    view! {
+                        cx,
+                        <Todo data = item />
+                    }
+                }
+            />
+        </section>
     }
 }
