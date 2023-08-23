159cf533218945092a288abcf63b4eaa2a22de97
ErneDainesi <edainesi@fi.uba.ar>
2023-08-23 20:58:01 -0300

    add db mod

---
+++ src/db.rs
@@ -0,0 +1,41 @@
+use anyhow::Ok;
+use sqlx::{Pool, Sqlite, SqlitePool};
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize, sqlx::FromRow)]
+pub struct TodoSchema {
+    pub title: String,
+    pub description: String,
+}
+
+pub async fn connect_to_db() -> anyhow::Result<SqlitePool> {
+    let db = SqlitePool::connect("sqlite::memory:").await?;
+    create_db(&db).await?;
+    Ok(db)
+}
+
+async fn create_db(db: &Pool<Sqlite>) -> anyhow::Result<()> {
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS todo (
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            title VARCHAR(250) NOT NULL,
+            description VARCHAR(250) NOT NULL
+        )",
+    )
+    .execute(db)
+    .await?;
+    Ok(())
+}
+
+pub async fn insert_todo(db: &Pool<Sqlite>, title: &str, description: &str) -> anyhow::Result<()> {
+    let insert =
+        format!("INSERT INTO todo (title, description) VALUES ('{title}', '{description}')");
+    sqlx::query(&insert).execute(db).await?;
+    Ok(())
+}
+
+pub async fn get_todos(db: &Pool<Sqlite>) -> anyhow::Result<Vec<TodoSchema>> {
+    let res = sqlx::query_as::<_, TodoSchema>("SELECT * FROM todo")
+        .fetch_all(db)
+        .await?;
+    Ok(res)
+}
